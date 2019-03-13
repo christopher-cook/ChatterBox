97ec551a60d1e41fb028eab86616a1d705ef9e3f
chef <cgcook81@gmail.com>
2019-03-13 17:26:47 -0400

    updated

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 
 class Chat extends Component {
   render() {
+    return (
     <h4>Individual Chat called here</h4>
+    )
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 
 class Header extends Component {
   render() {
+    return (
     <h2>Header here...</h2>
+  )
   }
 }
 

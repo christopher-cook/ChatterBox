7b89235a726a83e0deba5f3a27a0a0882846bede
chef <cgcook81@gmail.com>
2019-03-14 02:11:13 -0400

    react updated

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,8 +3,10 @@ import React, {Component} from 'react';
 class Chat extends Component {
   render() {
     return (
+      <div>
     <h4>Individual Chat called here</h4>
     {this.props.convoArray[0].name}
+    </div>
     )
   }
 }

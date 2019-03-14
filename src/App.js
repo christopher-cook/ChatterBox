727cb95373367bb547e0e96b29304ec453b2a02f
chef <cgcook81@gmail.com>
2019-03-14 02:14:41 -0400

    react updated

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     })
   }
   render() {
-    console.log(this.state.convoArray[0]["name"]);
+    console.log(this.state.convoArray);
     return (
       <div>
         <h1>Working React frontend</h1>

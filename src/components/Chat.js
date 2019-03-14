5340efdc2319c2ffcbb894b98f7b3bd452eb672a
chef <cgcook81@gmail.com>
2019-03-14 11:21:41 -0400

    react updated

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Chat extends Component {
   render() {
     return (
-      <div class ="chatz">
+      <div className ="chatz">
       {this.props.convo.name}<br />
       {this.props.convo.age}<br />
       {this.props.convo.fav_color}<br />

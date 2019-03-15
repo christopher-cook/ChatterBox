55127a5713d3fba66e2316236b65fa4e1b10f18d
chef <cgcook81@gmail.com>
2019-03-15 14:52:07 -0400

    updated create route

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,10 +20,12 @@ class Form extends Component {
   }
   render() {
     return (
+      <div>
       <form onSubmit={this.handleSubmit}>
         <input type="text" value={this.props.keyed_item} onChange={this.handleChange} placeholder="enter comment here..."/>
         <button type="submit" className="subButton">Submit</button>
       </form>
+      </div>
   )
   }
 }

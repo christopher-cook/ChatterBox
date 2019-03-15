ce5ba0114e797f9184c3b085f61cd7b7785f99e4
chef <cgcook81@gmail.com>
2019-03-15 14:22:15 -0400

    updated create route

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,7 +15,7 @@ class Form extends Component {
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.handleCreateChat(this.state.keyed_item);
+    this.props.handleCreateChat(keyed_item);
     this.clearForm()
   }
   render() {

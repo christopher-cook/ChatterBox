4e11caeac15ec1510d56d4d8b542b58ff60636c2
chef <cgcook81@gmail.com>
2019-03-15 12:52:57 -0400

    updated create route

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     })
     .then(data => {
       this.fetchData()
-      this.updateChatArray(data, convoArray)
+      this.updateChatArray(data, 'convoArray')
     })
     .catch(err => console.log(err))
   }

0fbe0d84cc750c4858f9b90bb1d8be68eaaf702b
chef <cgcook81@gmail.com>
2019-03-16 17:59:39 -0400

    updating create new chat convo

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Chat extends Component {
   render() {
     return (
-      <div className ="chatz">
+      <div>
       {this.props.convo.name}<br />
       {this.props.convo.feed}<br />
 

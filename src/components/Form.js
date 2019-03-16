0286620caf0dd1fa9b85b706a5786231845038b7
chef <cgcook81@gmail.com>
2019-03-16 18:44:23 -0400

    updating create new chat convo

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,8 +25,8 @@ class Form extends Component {
     return (
       <div>
       <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.props.name} onChange={this.handleChange} placeholder="commenter name" />
-        <input type="text" value={this.props.feed} onChange={this.handleChange} placeholder="enter comment here..." />
+        <input type="text" value={this.props.keyed_item} onChange={this.handleChange} placeholder="commenter name" />
+        <input type="text" value={this.props.keyed_item} onChange={this.handleChange} placeholder="enter comment here..." />
         <button type="submit" className="subButton">Submit</button>
       </form>
       </div>

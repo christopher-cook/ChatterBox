0a685c4d6bff15827048e7373080d9ee9b7ef89d
chef <cgcook81@gmail.com>
2019-03-17 21:46:53 -0400

    updated edit route

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,17 +19,16 @@ class Form extends Component {
   }
   handleSubmit = (event) => {
     event.preventDefault()
-    console.log(this.props.chat.id);
     if (this.state.submit === 'addChat') {
       this.props.createChat(this.state)
       this.clearForm()
   } else {
     let updatedChat = {
       id: this.props.chat.id,
-      name: this.props.convo.name,
-      feed: this.props.convo.feed
+      name: this.state.name,
+      feed: this.state.feed
     }
-    this.props.handleCheck(updatedChat, this.props.arrayIndex)
+    this.props.handleCheck(updatedChat, this.props.arrayIndex, 'convoArray')
     this.props.changeState()
   }
   }
@@ -37,9 +36,19 @@ class Form extends Component {
     return (
       <div>
       <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.name} id="name" onChange={this.handleChange} placeholder="commenter name" />
-        <input type="text" value={this.state.feed} id="feed" onChange={this.handleChange} placeholder="enter comment here..." />
-        <button type="submit" className="subButton">Submit</button>
+        <input type="text"
+          value={this.state.name}
+          id="name"
+          onChange={this.handleChange}
+          placeholder="commenter name" />
+
+        <input type="text"
+          value={this.state.feed}
+          id="feed"
+          onChange={this.handleChange}
+          placeholder="enter comment here..." />
+
+        <button type="submit">{this.state.submit}</button>
       </form>
       </div>
   )
